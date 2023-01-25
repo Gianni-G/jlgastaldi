951ef3d6850a4b591a6f4455b95b0d6a9428ed1a
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2023-01-25 00:51:34 -0500

    test menu buttons iphone

--- _site/assets/js/random-color.js
+++ _site/assets/js/random-color.js
@@ -1,5 +1,5 @@
 var colors = ['#3b4cc0', '#6282ea', '#8db0fe','#b8d0f9','#dddddd','#f5c4ad','#f49a7b','#de604d','#b40426'];
 var random_color = colors[Math.floor(Math.random() * colors.length)];
 document.getElementById("bla").style.backgroundColor = "red";
-document.p.style.backgroundColor = "red";
+document.p.style.color = "red";
 // $("#btn1").css("background-color",random_color);
