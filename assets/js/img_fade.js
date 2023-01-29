305d9b0f423d8e69a45d210eb8893ce77bfb7d70
Gianni-MB <juan.luis.gastaldi@gess.ethz.ch>
2023-01-29 12:59:39 -0500

    layout normalization 1

--- assets/js/img_fade.js
+++ assets/js/img_fade.js
@@ -7,11 +7,27 @@
 //      }
 //  });
 
- $(window).bind('scroll', function() {
-    if ($(window).scrollTop() > 0) {
-        $('#photo').hide();
-    }
+// $('photo').hide();
+// document.getElementsByClassName("image").style.visibility = "hidden";
+
+var photo = document.getElementsByClassName("image");
+
+photo[O].style.visibility = "hidden";
+
+// $(window).bind('scroll', function() {
+//     if ($(window).scrollTop() > 10) {
+//         photo[O].style.visibility = "hidden";
+//     }
+//     else {
+//         photo[O].style.visibility = "visible";
+//     }
+// });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop()> 10) {
+        photo[O].style.visibility = "hidden";
+     }
     else {
-        $('#photo').show();
-    }
-});
+      photo[O].style.visibility = "visible";
+     }
+ });
